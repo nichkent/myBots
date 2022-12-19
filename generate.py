75c89644fdbf1d73eed0817ffe06663a30de198d
nichkent <nikent@uvm.edu>
2022-12-19 15:40:38 -0500

    Changed starting position of cube so that it is at origin and ground level.

--- generate.py
+++ generate.py
@@ -6,6 +6,10 @@ length = 1
 width = 2
 height = 3
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[length, width, height])
+x = 0
+y = 1.5
+z = 0.5
+
+pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
 
 pyrosim.End()
