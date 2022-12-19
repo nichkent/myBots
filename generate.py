79ee846417e048b7e08ce191de31365230a24cf9
nichkent <nikent@uvm.edu>
2022-12-19 15:43:57 -0500

    Created two boxes. One of which starts just in front of and above the other.

--- generate.py
+++ generate.py
@@ -1,15 +1,17 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
 
 x = 0
-y = 1.5
+y = 0.5
 z = 0.5
 
 pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x + 1, z, y+1], size=[length, width, height])
+
 
 pyrosim.End()
