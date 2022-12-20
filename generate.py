99c1b078950c8706b1270f95847b6fcd71e58a89
nichkent <nikent@uvm.edu>
2022-12-20 12:27:13 -0500

    Made a bot with a front and back leg, and a torso.

--- generate.py
+++ generate.py
@@ -26,15 +26,13 @@ def Create_Robot():
     y = 0.5
     z = 0.5
 
-    pyrosim.Send_Cube(name="Link0", pos=[x, z, y], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
-    pyrosim.Send_Cube(name="Link1", pos=[x, z, y], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0, 0, 2])
-    pyrosim.Send_Cube(name="Link2", pos=[x, z, y-1], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[1, 0, 2])
-    pyrosim.Send_Cube(name="Link3", pos=[x, z, y-3], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link3_Link4", parent="Link3", child="Link4", type="revolute", position=[2, 0, 2])
-    pyrosim.Send_Cube(name="Link4", pos=[x-1, z, y - 5], size=[length, width, height])
+    pyrosim.Send_Cube(name="Torso", pos=[x + 1, z, y + 2], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[x - 1, z, y], size=[length, width, height])
+
+    pyrosim.Send_Cube(name="FrontLeg", pos=[x, z, y], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
 
     pyrosim.End()
 
