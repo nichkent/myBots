c8439031c91b019f234011293b5f98368e09933b
nichkent <nikent@uvm.edu>
2022-12-21 11:06:02 -0500

    Values for the back legs touch are printing to the console.

--- simulate.py
+++ simulate.py
@@ -16,8 +16,10 @@ pyrosim.Prepare_To_Simulate(robotId)
 
 for i in range(1000):
     p.stepSimulation()
+
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    print(backLegTouch)
+
     t.sleep(1/60)
-    print(i)
 
 p.disconnect()
