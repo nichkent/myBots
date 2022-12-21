2bcf559a15875d5335db95dd18d709888e447443
nichkent <nikent@uvm.edu>
2022-12-21 11:15:35 -0500

    Added numpy for collection and saving of data across simulations.

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time as t
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -13,13 +14,14 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorsValues = numpy.zeros(100)
 
-for i in range(1000):
+for i in range(100):
     p.stepSimulation()
 
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorsValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 
     t.sleep(1/60)
 
+numpy.save("data/backLegTouchVector.npy", backLegSensorsValues[i])
 p.disconnect()
