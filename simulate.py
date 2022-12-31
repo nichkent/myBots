8c391ca61de328f80889c51fe767fe9417d5b145
nichkent <nikent@uvm.edu>
2022-12-30 20:14:37 -0500

    Robot is officially moving!

--- simulate.py
+++ simulate.py
@@ -1,10 +1,19 @@
+import numpy as np
 import pybullet as p
 import time as t
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
-import math
-import random
+
+# Wave variables
+amplitudeBackLeg = numpy.pi/4
+frequencyBackLeg = 10
+phaseOffsetBackLeg = 0
+
+amplitudeFrontLeg = numpy.pi/4
+frequencyFrontLeg = 10
+phaseOffsetFrontLeg = 0
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -16,36 +25,47 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorsValues = numpy.zeros(500)
-frontLegSensorsValues = numpy.zeros(500)
+backLegSensorsValues = numpy.zeros(1000)
+frontLegSensorsValues = numpy.zeros(1000)
 
-for i in range(500):
+
+for i in range(1000):
     # Makes the simulation step one time (tic)
     p.stepSimulation()
 
     # Stores sensor values
     backLegSensorsValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
     frontLegSensorsValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    # Store targetPosition Values
+    backLegTargetAngles = amplitudeBackLeg * numpy.sin(frequencyBackLeg * i + phaseOffsetBackLeg)
+    frontLegTargetAngles = amplitudeFrontLeg * numpy.sin(frequencyFrontLeg * i + phaseOffsetFrontLeg)
+    #numpy.save("data/sinAngles.npy", sinAngles)
 
     # Supplies force to the backleg joint
     pyrosim.Set_Motor_For_Joint(
         bodyIndex=robotId,
         jointName=b'Torso_BackLeg',
         controlMode=p.POSITION_CONTROL,
-        targetPosition=((math.pi * random.random()) - math.pi/2),
+        targetPosition=amplitudeBackLeg * numpy.sin(frequencyBackLeg * i + phaseOffsetBackLeg),
         maxForce=500)
+
 
     # Supplies force to the front joint
     pyrosim.Set_Motor_For_Joint(
         bodyIndex=robotId,
         jointName=b'Torso_FrontLeg',
         controlMode=p.POSITION_CONTROL,
-        targetPosition=((math.pi * random.random()) - math.pi/2),
+        targetPosition=amplitudeFrontLeg * numpy.sin(frequencyFrontLeg * i + phaseOffsetFrontLeg),
         maxForce=500)
 
     # Makes the programs in simulation time run at 1/60 of a sec
     t.sleep(1/60)
 
+
 numpy.save("data/backLegTouchVector.npy", backLegSensorsValues)
 numpy.save("data/frontLegTouchVector.npy", frontLegSensorsValues)
+numpy.save("data/backLegTargetPosition.npy", backLegTargetAngles)
+numpy.save("data/frontLegTargetPosition.npy", frontLegTargetAngles)
 p.disconnect()
