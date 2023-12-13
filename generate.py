6349df5da766217ff6e037479c0c0ecf6a343456
Nicholas Kent <icedragonoqs@gmail.com>
2023-12-13 16:01:05 -0500

    Fixed the y, z variables so they are now modifying the correct values. Created a 5x5 tower of blocks.

--- generate.py
+++ generate.py
@@ -10,18 +10,49 @@ height = 1
 
 # Define cube 1 pos
 x = 0
-y = 0
-z = 0.5
+y = 0.5
+z = 0
 
 
 
 # Floor loop for cube tower
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-    z = z + 1
-    length = length * .9
-    width = width * .9
-    height = height * .9
+# Creates the rows length-wise
+for i in range(5):
+    # Reset positions
+    y = 0.5
+    z = 0
+
+    # Increment row length-wise on each iteration
+    x = x + 1
+
+    # Reset the dimensions of each tower's bottom cube
+    length = 1
+    width = 1
+    height = 1
+    # Creates the rows width-wise
+    for j in range(5):
+        # Reset position
+        y = 0.5
+
+        # Increment row width-wise on each iteration
+        z = z + 1
+
+        # Reset the dimensions of each tower's bottom cube
+        length = 1
+        width = 1
+        height = 1
+        # Creates the height
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x, z, y], size=[length, width, height])
+
+            # Increment height pos of cube
+            y = y + 1
+
+            # Make each cube on top 90% of the dims of the cube below it
+            length = length * .9
+            width = width * .9
+            height = height * .9
+
 
 
 # End generation
