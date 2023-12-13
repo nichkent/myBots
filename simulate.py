2b8a99b496268271c5739d96c69415e4dfd7b42a
Nicholas Kent <icedragonoqs@gmail.com>
2023-12-13 12:03:31 -0500

    Stepped through the simulation 1000.

--- simulate.py
+++ simulate.py
@@ -1,9 +1,14 @@
+import time
 import pybullet as p
 
 # Start the sim
 physicsClient = p.connect(p.GUI)
 
-
+# Step the simulation 1000 times
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1)
+    print("Iteration:", i)
 
 # Stop sim
 p.disconnect()
