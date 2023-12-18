d3d3747f950a0d762a6932f0c209e7db2066932f
Nicholas Kent <icedragonoqs@gmail.com>
2023-12-18 15:59:24 -0500

    Fixed an issue with setting the motorValues list. Now is able to control each joint separately.

--- constants.py
+++ constants.py
@@ -1,11 +1,6 @@
 import numpy
 
-
 # Motor values
 amplitude = numpy.pi/6
-frequency = 20
+frequency = 5
 phaseOffset = numpy.pi/4
-
-
-# Create the vectors for movement based on a sin wave
-targetAngles = amplitude * numpy.sin(2 * numpy.pi * frequency * numpy.linspace(0, 1, 1000) + phaseOffset)
--- motor.py
+++ motor.py
@@ -13,17 +13,27 @@ class MOTOR:
         # Using the current robot id
         self.robotId = robotId
 
-        # Create a np vector filled with zeros based on a sin function defined in constants.py
-        self.motorValues = c.targetAngles
 
         # Call Prepare_To_Act in motor.py
         self.Prepare_To_Act()
 
+        # Create a np vector filled with zeros based on a sin function defined in constants.py
+        self.motorValues = self.targetAngles
+
     def Prepare_To_Act(self):
-        # Pull constants from constants.py
-        self.amplitude = c.amplitude
-        self.frequency = c.frequency
-        self.offset = c.phaseOffset
+        if self.jointName == "Torso_FrontLeg":
+            # Pull constants from constants.py
+            self.amplitude = c.amplitude
+            self.frequency = c.frequency
+            self.offset = c.phaseOffset
+        if self.jointName == "Torso_BackLeg":
+            # Pull constants from constants.py
+            self.amplitude = c.amplitude
+            self.frequency = c.frequency / 2
+            self.offset = c.phaseOffset
+
+        # Create the vectors for movement based on a sin wave
+        self.targetAngles = self.amplitude * numpy.sin(2 * numpy.pi * self.frequency * numpy.linspace(0, 1, 1000) + self.offset)
 
     def Set_Value(self, t, robotId):
         # Create motor for the robot's joints
