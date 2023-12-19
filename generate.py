4f505c33e3ffa8e3177d1c68b2fe24f1be47d58f
Nicholas Kent <icedragonoqs@gmail.com>
2023-12-19 18:16:55 -0500

    Added neurons to the front leg so that it can move in similar functionality to the back leg.

--- generate.py
+++ generate.py
@@ -72,6 +72,8 @@ def Generate_Brain():
     # Generate synapses
     pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=1.0)
     pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1.0)
+    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=-1.0)
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=-1.0)
 
 
     # End robot generation
